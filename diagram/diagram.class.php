b013631c6e27c590bef1bd677218da18fd26be3c
Михаил Кройтор <m_croitor@hotmail.com>
2016-06-27 14:24:01 +0300

    diagram generation was rewritten with classes

--- diagram/diagram.class.php
+++ diagram/diagram.class.php
@@ -1,66 +1,51 @@
 <?php
 
-class Diagram {
+$colors = array(
+    "black" => array(0, 0, 0),
+    "red" => array(255, 0, 0),
+    "green" => array(0, 255, 0),
+    "blue" => array(0, 0, 255),
+    "gray" => array(127, 127, 127),
+    "darkred" => array(127, 0, 0),
+    "darkgreen" => array(0, 127, 0),
+    "darkblue" => array(0, 0, 127),
+    "white" => array(255, 255, 255),
+    "blackshift" => array(127, 127, 127),
+    "redshift" => array(255, 127, 127),
+    "greenshift" => array(127, 255, 127),
+    "blueshift" => array(127, 127, 255),
+    "grayshift" => array(196, 196, 196),
+    "darkredshift" => array(196, 127, 127),
+    "darkgreenshift" => array(127, 196, 127),
+    "darkblueshift" => array(127, 127, 196)
+);
+
+//conversion pixel -> millimeter at 72 dpi
+function px2mm($px) {
+    return $px * 25.4 / 72;
+}
+
+//conversion millimeter at 72 dpi -> pixel
+function mm2px($mm) {
+    return $mm * 72 / 25.4;
+}
+
+//conversion pixel -> point
+function px2pt($px) {
+    return $px * 3 / 4;
+}
+
+//conversion point -> pixel
+function pt2px($pt) {
+    return $pt * 4 / 3;
+}
+
+class Board {
 
     var $board;
-    var $style;
-    var $size;
 
-    function __construct($fen, $options = array()) {
-        $o = filter_var($options, FILTER_DEFAULT, array("options" => array(
-                "default" => array("style" => "alpha", "size" => 30)
-        )));
+    function __construct($fen) {
         $this->board = $this->fen2board($fen);
-        $this->style = filter_var($o["style"], FILTER_DEFAULT, array("options" => array("default" => "alpha")));
-        $this->size = filter_var($o["size"], FILTER_VALIDATE_INT, array("options" => array(
-                "default" => 30,
-                "min_range" => 10,
-                "max_range" => 512
-        )));
-    }
-
-    function ascii($s) {
-        $str = $s["top"] . "\n";
-        for ($i = 0; $i !== 8; ++$i) {
-            $str .= $s["|"];
-            for ($j = 0; $j !== 8; ++$j) {
-                if ($this->board[$i][$j] == " ") {
-                    $index = ($i % 2 === $j % 2) ? "+" : "-";
-                } else {
-                    $index = ($i % 2 === $j % 2) ? $this->board[$i][$j] . "0" : $this->board[$i][$j] . "1";
-                }
-
-                $str .= $s[$index];
-            }
-            $str .= $s["/"] . "\n";
-        }
-        $str .= $s["bottom"];
-        return $str;
-    }
-
-    private function is_correct_epd($epd) {
-        $len = strlen($epd);
-        $fields = 0;
-        for ($i = 0; $i < $len; $i++) {
-            if (strpos("12345678", $epd{$i}) === true) {
-                $fields +=$epd{$i};
-            } elseif (strpos("kqrbnpKQRBNPX", $epd{$i}) === true) {
-                $fields++;
-            } elseif ($epd{$i} === '/') {
-                
-            } else {
-                $fields +=100;
-            }
-        }
-        return $fields === 64;
-    }
-
-    function toString() {
-        $str = "";
-        for ($i = 0; $i != 8; ++$i) {
-            $str .= implode($this->board[$i]) . "\n";
-        }
-        return $str;
     }
 
     function fen2board($fen) {
@@ -99,9 +84,141 @@ class Diagram {
         return $brd_line;
     }
 
-    function toImage(array $options) {
-        $is_solid = filter_var($options["solid"], FILTER_VALIDATE_BOOLEAN, array("options" => array("default" => false)));
-        $is_double = filter_var($options["double"], FILTER_VALIDATE_BOOLEAN, array("options" => array("default" => false)));
+    private function is_correct_epd($epd) {
+        $len = strlen($epd);
+        $fields = 0;
+        for ($i = 0; $i < $len; $i++) {
+            if (strpos("12345678", $epd{$i}) === true) {
+                $fields +=$epd{$i};
+            } elseif (strpos("kqrbnpKQRBNPX", $epd{$i}) === true) {
+                $fields++;
+            } elseif ($epd{$i} === '/') {
+                
+            } else {
+                $fields +=100;
+            }
+        }
+        return $fields === 64;
+    }
+
+    function toString() {
+        $str = "";
+        for ($i = 0; $i != 8; ++$i) {
+            $str .= implode($this->board[$i]) . "\n";
+        }
+        return $str;
+    }
+
+    function lines() {
+        $l = array();
+        for ($i = 0; $i != 8; ++$i) {
+            $l[$i] = implode($this->board[$i]);
+        }
+        return $l;
+    }
+
+    function ascii($simbols) {
+        $result = "";
+        for ($i = 0; $i !== 8; ++$i) {
+            $board_line = array();
+            for ($j = 0; $j !== 8; ++$j) {
+                if ($this->board[$i][$j] === " ") {
+                    $index = ($i % 2 === $j % 2) ? "+" : "-";
+                } else {
+                    $index = ($i % 2 === $j % 2) ? $this->board[$i][$j] . "0" : $this->board[$i][$j] . "1";
+                }
+
+                $board_line[$j] = $simbols[$index];
+            }
+            $result .= implode($board_line) . "\n";
+        }
+        return $result;
+    }
+
+}
+
+class Diagram {
+
+    var $board;
+    var $style, $solid, $dbl_margin, $color, $size;
+    var $simbols;
+    var $m, $b, $s;
+
+    function __construct($fen, $options = array()) {
+        $this->board = new Board($fen);
+        $this->style = empty($options["style"]) ? "alpha" : $options["style"];
+        $this->size = empty($options["size"]) ? 30 : $options["size"];
+        $this->color = empty($options["color"]) ? "black" : $options["color"];
+        $this->solid = empty($options["solid"]) ? false : $options["solid"];
+        $this->dbl_margin = empty($options["dbl_margin"]) ? "true" : $options["dbl_margin"];
+
+        $this->s = 8 * $this->size;
+        $this->b = (int) ($this->size / 20) + 1;
+        $this->m = $this->b + 1;
+        $this->m *= ($this->dbl_margin === true) ? 2 : 1;
+        $this->s += 2 * $this->m;
+
+        if (!file_exists("./font-desc/{$this->style}.php")) {
+            $this->style = "alpha";
+        }
+        include_once("./font-desc/{$this->style}.php");
+        global $simbols;
+        $this->simbols = $simbols[$this->style];
+    }
+
+    function ascii() {
+        return $this->board->ascii($this->simbols);
+    }
+
+    function toImage() {
+        global $colors;
+
+        $d = imagecreatetruecolor($this->s + 1, $this->s + 1); // +1 fix
+        $dark_color = imagecolorallocate(
+                $d, $colors[$this->color][0], $colors[$this->color][1], $colors[$this->color][2]);
+        $light_color = imagecolorallocate(
+                $d, $colors[$this->color . "shift"][0], $colors[$this->color . "shift"][1], $colors[$this->color . "shift"][2]);
+        $white_color = imagecolorallocate(
+                $d, $colors["white"][0], $colors["white"][1], $colors["white"][2]);
+
+        imagefill($d, 0, 0, $dark_color);
+        imagefilledrectangle(
+                $d, $this->b, $this->b, $this->s - $this->b, $this->s - $this->b, $white_color);
+
+        if ($this->dbl_margin === true) {
+            imagefilledrectangle(
+                    $d, 2 * $this->b, 2 * $this->b, $this->s - 2 * $this->b, $this->s - 2 * $this->b, $dark_color);
+            imagefilledrectangle(
+                    $d, 3 * $this->b, 3 * $this->b, $this->s - 3 * $this->b, $this->s - 3 * $this->b, $white_color);
+        }
+
+        if ($this->solid == true) {
+            for ($i = 0; $i != 8; ++$i) {
+                for ($j = 0; $j !== 8; ++$j) {
+                    if ($i % 2 !== $j % 2) {
+                        imagefilledrectangle(
+                                $d, $i * $this->size + $this->m, $j * $this->size + $this->m, ($i + 1) * $this->size + $this->m, ($j + 1) * $this->size + $this->m, $light_color);
+                    } else {
+                        imagefilledrectangle(
+                                $d, $i * $this->size + $this->m, $j * $this->size + $this->m, ($i + 1) * $this->size + $this->m, ($j + 1) * $this->size + $this->m, $white_color);
+                    }
+                }
+            }
+        }
+
+        for ($i = 0; $i != 8; ++$i) {
+            for ($j = 0; $j != 8; ++$j) {
+                $index = ($i % 2 === $j % 2) ? "0" : "1";
+                $char = $this->board->board[$i][$j];
+                if ($char === " ") {
+                    $char = ($index === "0") ? $this->simbols["-"] : $this->simbols["+"];
+                } else {
+                    $char = $this->simbols[$char . $index];
+                }
+                imagefttext($d, px2pt($this->size), 0, $j * $this->size + $this->m, ($i+1) * $this->size + $this->m, $dark_color, "./fonts/{$this->style}.ttf", $char);
+            }
+        }
+        return $d;
     }
 
 }
--- diagram/index.php
+++ diagram/index.php
@@ -1,29 +1,5 @@
 <?php
 
-// introduce into font description
-$delta = array(
-    "alpha" => 0,
-    "alpha2" => -7,
-    "aventurer" => 0,
-    "berlin" => 0,
-    "case" => 0,
-    "cheq" => -4,
-    "chess7" => 0,
-    "goodcompanions" => -7,
-    "hastings" => -8,
-    "isdiagram" => 0,
-    "kingdom" => 0,
-    "leipzig" => 0,
-    "mark" => 0,
-    "marroquin" => 0,
-    "maya" => 0,
-    "merida" => 0,
-    "milenia" => 0,
-    "motif" => 0,
-    "spsl" => -8,
-    "usual" => 0,
-);
-
 $fen = filter_input(INPUT_GET, "fen");
 if ($fen === false) {
     exit("<b>using</b>:<br />index.php?fen=&lt;FEN&gt;");
@@ -36,164 +12,21 @@ $size = filter_input(INPUT_GET, 'size', FILTER_VALIDATE_INT, array("options" => 
         )));
 $style = filter_input(INPUT_GET, "style", FILTER_DEFAULT, array("options" => array("default" => "alpha")));
 $solid = filter_input(INPUT_GET, "solid", FILTER_VALIDATE_BOOLEAN, array("options" => array("default" => false)));
+$dbl_margin = filter_input(INPUT_GET, "double", FILTER_VALIDATE_BOOLEAN, array("options" => array("default" => false)));
 $color = filter_input(INPUT_GET, "color", FILTER_DEFAULT, array("options" => array("default" => "black")));
 
-if (file_exists("./font-desc/{$style}.php")) {
-    include_once("./font-desc/{$style}.php");
-} else {
-    include_once("./font-desc/alpha.php");
-}
+$options = array(
+    "size" => $size,
+    "style" => $style,
+    "solid" => $solid,
+    "color" => $color,
+    "dbl_margin" => $dbl_margin
+);
 
-//conversion pixel -> millimeter at 72 dpi
-function px2mm($px) {
-    return $px * 25.4 / 72;
-}
+include './diagram.class.php';
 
-//conversion millimeter at 72 dpi -> pixel
-function mm2px($mm) {
-    return $mm * 72 / 25.4;
-}
-
-//conversion pixel -> point
-function px2pt($px) {
-    return $px * 3 / 4;
-}
-
-//conversion point -> pixel
-function pt2px($pt) {
-    return $pt * 4 / 3;
-}
-
-function is_correct_epd($epd) {
-    $len = strlen($epd);
-    $fields = 0;
-    for ($i = 0; $i < $len; $i++) {
-        if (strpos("12345678", $epd{$i}) !== false) {
-            $fields +=$epd{$i};
-        } elseif (strpos("kqrbnpKQRBNPX", $epd{$i}) !== false) {
-            $fields++;
-        } elseif ($epd{$i} === '/') {
-            
-        } else {
-            $fields +=100;
-        }
-    }
-    return $fields === 64;
-}
-
-function fen2board($fen, $solid) {
-    global $simbols;
-    // cut the tail if exist
-    $parts = explode(" ", $fen);
-    $epd = $parts[0];
-
-    // fen string verifying
-    if (is_correct_epd($epd) === true) {
-        $epd = "8/8/8/8/8/8/8/8";
-    }
-    $len = strlen($epd);
-
-    // building diagram
-    $boardline = "";
-
-    $fields = 0;
-    for ($i = 0; $i < $len; $i++) {
-        if (strpos("kqrbnpKQRBNPX", $epd{$i}) !== false) {
-            $posy = $fields >> 3;
-            $posx = $fields % 8;
-
-            $index = ($posy % 2 == $posx % 2) ? "0" : "1";
-            $boardline .= $simbols[$style][$epd{$i} . $index];
-            if (($fields + 1) % 8 === 0) {
-                $board[] = ($solid === true) ? strtolower($boardline) : $boardline;
-                $boardline = "";
-            }
-            $fields++;
-        } elseif ($epd{$i} > "0" and $epd{$i} < "9") {
-            for ($j = 0; $j < $epd{$i}; $j++) {
-                $posy = $fields >> 3;
-                $posx = $fields % 8;
-                if ($solid === true) {
-                    $boardline .= $simbols[$style]["-"];
-                } else {
-                    $boardline .= ($posy % 2 === $posx % 2) ? $simbols[$style]["-"] : $simbols[$style]["+"];
-                }
-                if (($fields + 1) % 8 === 0) {
-                    //$board[] = $boardline;
-                    $board[] = ($solid === true) ? strtolower($boardline) : $boardline;
-                    $boardline = "";
-                }
-                $fields++;
-            }
-        }
-    }
-
-    return $board;
-}
-
-function board2diag($board, $size, $color, $solid) {
-    global $style;
-    $diagram = imagecreatetruecolor($size * 8 + 7, $size * 8 + 7);
-
-    $colors = array(
-        "black" => imagecolorallocate($diagram, 0, 0, 0),
-        "red" => imagecolorallocate($diagram, 255, 0, 0),
-        "green" => imagecolorallocate($diagram, 0, 255, 0),
-        "blue" => imagecolorallocate($diagram, 0, 0, 255),
-        "gray" => imagecolorallocate($diagram, 127, 127, 127),
-        "darkred" => imagecolorallocate($diagram, 127, 0, 0),
-        "darkgreen" => imagecolorallocate($diagram, 0, 127, 0),
-        "darkblue" => imagecolorallocate($diagram, 0, 0, 127),
-        "white" => imagecolorallocate($diagram, 255, 255, 255),
-        "blackshift" => imagecolorallocate($diagram, 127, 127, 127),
-        "redshift" => imagecolorallocate($diagram, 255, 127, 127),
-        "greenshift" => imagecolorallocate($diagram, 127, 255, 127),
-        "blueshift" => imagecolorallocate($diagram, 127, 127, 255),
-        "grayshift" => imagecolorallocate($diagram, 196, 196, 196),
-        "darkredshift" => imagecolorallocate($diagram, 196, 127, 127),
-        "darkgreenshift" => imagecolorallocate($diagram, 127, 196, 127),
-        "darkblueshift" => imagecolorallocate($diagram, 127, 127, 196)
-    );
-
-    imagefill($diagram, 0, 0, $colors[$color]);
-    imagefilledrectangle(
-            $diagram, 2, 2, $size * 8 + 4, $size * 8 + 4, $colors["white"]);
-
-    imagefilledrectangle(
-            $diagram, 3, 3, $size * 8 + 3, $size * 8 + 3, $colors[$color]);
-    imagefilledrectangle(
-            $diagram, 4, 4, $size * 8 + 2, $size * 8 + 2, $colors["white"]);
-
-    if ($solid === true) {
-        for ($i = 0; $i != 8; ++$i) {
-            for ($j = 0; $j !== 8; ++$j) {
-                if ($i % 2 !== $j % 2) {
-                    imagefilledrectangle(
-                            $diagram, $i * $size + 3, $j * $size + 3, ($i + 1) * $size + 3, ($j + 1) * $size + 3, $colors[$color . "shift"]);
-                } else {
-                    imagefilledrectangle(
-                            $diagram, $i * $size + 3, $j * $size + 3, ($i + 1) * $size + 3, ($j + 1) * $size + 3, $colors["white"]);
-                }
-            }
-        }
-    }
-
-    foreach ($board as $index => $line) {
-        //imagefttext($diagram, px2pt($size), 0, 0, ($index + 1) * $size, $colors[$color], "./fonts/$style.ttf", $line);
-        imagefttext($diagram, px2pt($size), 0, 3, ($index + 1) * $size + 3 + $simbols["delta"], $colors[$color], "./fonts/$style.ttf", $line);
-    }
-    return $diagram;
-}
-
-function fen2diag($fen, $size, $color, $solid) {
-    global $style;
-    $board = fen2board($fen, $style, $solid);
-    $diagram = board2diag($board, $style, $size, $color, $solid);
-
-    return $diagram;
-}
-
-$src = fen2diag($fen, $style, $size, $color, $solid);
+$d = new Diagram($fen, $options);
 
 header('Content-type: image/png');
-imagepng($src);
+imagepng($d->toImage());
+
